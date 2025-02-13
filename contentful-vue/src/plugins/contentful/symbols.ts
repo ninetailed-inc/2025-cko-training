e0200a6a4cff52496b90b165d98b4004377ffe06
Chuck Kosman <chuck@ninetailed.com>
2025-02-13 23:16:47 +0100

    Hoist project to root; deploy button update

--- contentful-vue/src/plugins/contentful/symbols.ts
+++
@@ -1,5 +0,0 @@
-// symbols.ts
-import type { ContentfulClientApi } from 'contentful'
-import type { InjectionKey } from 'vue'
-
-export const ContentfulKey: InjectionKey<ContentfulClientApi<undefined>> = Symbol('Contentful')
--- marketing-contentful-next-app/app/api/exit-draft/route.ts
+++
@@ -1,6 +0,0 @@
-import { draftMode } from 'next/headers';
-
-export function GET() {
-  draftMode().disable();
-  return new Response('Draft mode is disabled');
-}
